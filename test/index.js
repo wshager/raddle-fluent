8b04eebd611505b9477a1e112139692ce4a9f396
W.S. Hager <wshager@gmail.com>
2018-10-23 23:40:48 +0200

    expose rql through module factory

--- test/index.js
+++ test/index.js
@@ -1,28 +1,24 @@
 const core = require("../src/index").core;
 
-let $ = core();
 
-// TODO:
-// - assume exports in module namespace (we're not exporting smthg else!)
-// - use default prefix for exposing
+const rql = () => {
+	let $ = core();
+	return $ = $.export("aggregate",
+		$ => $.function($ => $.function($ => $.any($ => $.object()), $ => $.any($ => $.object())), $ => $.any($ => $.object()))
+	).export("match",
+		$ => $.function($ => $.function($ => $.object(), $ => $.boolean()), $ => $.function($ => $.any($ => $.object()), $ => $.any($ => $.object())))
+	).export("where",
+		$ => $.function($ => $.string().function($ => $.atomic(), $ => $.boolean()), $ => $.function($ => $.object(), $ => $.boolean()))
+	).export("eq",
+		$ => $.function($ => $.atomic(), $ => $.function($ => $.atomic(), $ => $.boolean()))
+	).export("eq",
+		$ => $.function($ => $.string().atomic(), $ => $.function($ => $.object(), $ => $.boolean()))
+	).export("and",
+		$ => $.function($ => $.function($ => $.object(), $ => $.boolean()).function($ => $.object(), $ => $.boolean()).restParams(), $ => $.function($ => $.object(), $ => $.boolean()))
+	).$module("aggregate#1");
+};
 
-$ = $.export("aggregate",
-	$ => $.function($ => $.function($ => $.any($ => $.object()), $ => $.any($ => $.object())), $ => $.any($ => $.object()))
-).export("match",
-	$ => $.function($ => $.function($ => $.object(), $ => $.boolean()), $ => $.function($ => $.any($ => $.object()), $ => $.any($ => $.object())))
-).export("where",
-	$ => $.function($ => $.string().function($ => $.atomic(), $ => $.boolean()), $ => $.function($ => $.object(), $ => $.boolean()))
-).export("eq",
-	$ => $.function($ => $.atomic(), $ => $.function($ => $.atomic(), $ => $.boolean()))
-).export("eq",
-	$ => $.function($ => $.string().atomic(), $ => $.function($ => $.object(), $ => $.boolean()))
-).export("and",
-	$ => $.function($ => $.function($ => $.object(), $ => $.boolean()).function($ => $.object(), $ => $.boolean()).restParams(), $ => $.function($ => $.object(), $ => $.boolean()))
-);
-
-$ = $.$setDefaultPrefix("rql").$expose("aggregate#1");
-
-$ = $
+const $ = rql()
 	.aggregate()
 	.match()
 	.and()
